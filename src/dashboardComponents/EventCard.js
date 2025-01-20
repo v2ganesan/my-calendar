e629777d56305ed29dd53a86fee0af6c2b08d93b
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-01-20 13:39:58 -0800

    finished event retrieval route

--- src/dashboardComponents/EventCard.js
+++ src/dashboardComponents/EventCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './dashboard.css';
 
-export default function EventCard() {
+export default function EventCard({title, duration, location}) {
     return (
       <div className="event-card">
         <div className="event-card-header">
           <input type="checkbox" />
-          <span className="event-title">30 Minute Meeting</span>
+          <span className="event-title">{title}</span>
           <span className="settings-icon">⚙️</span>
         </div>
         <div className="event-card-body">
-          <p><strong>30 min</strong>, One-on-One</p>
+          <p><strong>{duration}</strong>, {location}</p>
           <a href="#" className="booking-page-link">View booking page</a>
         </div>
         <div className="event-card-footer">
