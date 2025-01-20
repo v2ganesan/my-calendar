852dc4db1c45c70eaa0b621394b7cd99977b5c35
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-01-19 17:43:30 -0800

    google logout button

--- src/dashboardComponents/Header.js
+++ src/dashboardComponents/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './dashboard.css'
+import { googleLogout } from '@react-oauth/google';
+import { useNavigate } from "react-router-dom";
+
+
+
 
 export default function Header({ name, profilePicture }) {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Redirect to login page
+        googleLogout();
+        navigate("/", { state: null });
+      };
+      
     return (
       <div className="header-container">
+        <button onClick={handleLogout}> Logout </button>
         <img
           src={profilePicture}
           alt="pfp"
