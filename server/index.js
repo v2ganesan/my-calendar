15d0fc8fce500b8abc569bce3c17f98ef0c3eded
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-02-03 22:18:32 -0800

    updates to middleware

--- server/index.js
+++ server/index.js
@@ -5,17 +5,47 @@ const sequelize = require('./config/database');
 const userRoutes = require('./routes/users');
 const eventRoutes = require('./routes/eventOps')
 const apptRoutes = require('./routes/apptOps')
-
+const loginRoute = require('./routes/googleAuth')
+const path = require('path');
+const { GoogleOAuthProvider } = require('@react-oauth/google');
 const app = express();
 
+
+// Detailed CORS configuration
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  credentials: true, // Allow credentials (cookies, authorization headers)
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use((req, res, next) => {
+  //res.removeHeader('Cross-Origin-Opener-Policy');  // Remove any existing COOP headers
+  //res.header('Cross-Origin-Opener-Policy', 'unsafe-none');  // Set to most permissive option
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  //res.header('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  res.header('Cross-Origin-Resource-Policy', 'cross-origin');
+  next();
+});
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/eventOps', eventRoutes);
-app.use('/api/apptOps', apptRoutes)
+app.use('/api/apptOps', apptRoutes);
+app.use('/api/auth', loginRoute);
+
+// allow express to serve static files 
+app.use(express.static(path.join(__dirname, '../build')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 
 // Database connection test
