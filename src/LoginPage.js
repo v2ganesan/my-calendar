62ea66394094e1be72439c2ede6e74507ef8e5d3
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-02-03 22:19:39 -0800

    starting oauth flow when login button is clicked

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,59 +1,17 @@
 import React from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import { redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
 export default function LoginPage() {
-  const navigate = useNavigate();
-
-  const handleSuccess = async (credentialResponse) => {
-    try {
-      // Use jwtDecode to get the user's info 
-      const decoded = jwtDecode(credentialResponse.credential);
-      const { email, name, picture } = decoded;
-
-      console.log("Checking user:", email);
-      console.log(picture)
-
-      const response = await fetch(`/api/users/check?email=${email}`);
-      const data = await response.json();
-
-      console.log("Server response:", data);
-      console.log(picture)
-      if (data.isNewUser) {
-        navigate('/setup', { 
-          state: { 
-            email, 
-            name 
-          } 
-        });
-      } else {
-        navigate('/success', {
-          state: {
-            email,
-            name, 
-            picture
-          }
-        });
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-    }
-  };
-
-  const handleFailure = () => {
-    console.error("Login Failed");
-  };
-
   return (
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <div style={{ textAlign: "rght", marginTop: "50px" }}>
         <h1>Welcome to Calend-Ez!</h1>
       <h1>Sign Up</h1>
-        <GoogleLogin
-          onSuccess={handleSuccess}
-          onError={handleFailure}
-        />
+        <button onClick={() => { window.location.href = 'http://localhost:3001/api/auth/google'; }}>
+          Sign In
+      </button>
       </div>
     </GoogleOAuthProvider>
   );
