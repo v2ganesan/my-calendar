bd8b4edf9ea0fd3f4ef45114b0efbfd0fdcfb40e
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-02-08 15:26:15 -0800

    find all possible slots

---
+++ server/utils/slotGenerator.js
@@ -0,0 +1,24 @@
+export async function generatePossibleSlots(startTime, endTime, duration) {
+  //array of possible slots to check for overlap with existing appts 
+  const slots = [];
+  //console.log('start', startTime);
+  let current = new Date(`1970-01-01T${startTime}`);
+  //console.log('current', current );
+  const end = new Date(`1970-01-01T${endTime}`);
+  console.log('duration', duration.hours);
+  const durationIncrement = duration.hours * 60 * 60 * 1000;
+
+    while(current < end) {
+      const slotEnd = new Date(current.getTime() + durationIncrement);
+      console.log('end time', slotEnd)
+      if(slotEnd <= end) {
+        console.log('Slot start ', current.toTimeString().substring(0, 5));
+        slots.push({
+          start: current.toTimeString().substring(0, 5),
+          end: slotEnd.toTimeString().substring(0, 5)
+        });
+      }
+      current = slotEnd;
+    }
+    return slots;
+  }
