10cde8d0f7a10bb256b8c6342b72b1d62a372876
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-02-12 23:05:27 -0800

    implementing checkExisting route before booking appt

--- src/bookingPage/ConfirmBooking.js
+++ src/bookingPage/ConfirmBooking.js
@@ -23,9 +23,17 @@ export default function ConfirmBooking() {
     // Use the function with duration treated as hours
     const endTime = calculateEndTime(slot, Number(duration));
 
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
+            const isSlotAvailable = await fetch(`/api/apptOps/checkExisting/${slot}`);
+            const isAvailable = await isSlotAvailable.json();
+            if (isAvailable) {
+                alert('Slot is already booked.');
+                return;
+            }
+
             const response = await fetch('/api/apptOps/createAppointment', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
@@ -53,6 +61,7 @@ export default function ConfirmBooking() {
     return (
         <form onSubmit={handleSubmit}>
             <h1>Confirm Your Appointment</h1>
+            
             <label>
                 Name:
                 <input type="text" value={attendeeName} onChange={(e) => setAttendeeName(e.target.value)} required />
