e5fd695f8071a84f393f619aca0da15ecd416f76
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-02-14 00:01:11 -0800

    nav to select between events/appts dashboard

--- src/dashboardComponents/Sidebar.js
+++ src/dashboardComponents/Sidebar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './dashboard.css';
-
-export default function Sidebar() {
+import { Link } from 'react-router-dom';
+export default function Sidebar({email}) {
+  console.log("the email iofeuhiueqg,", email);
   return (
     <div className="sidebar">
       <h2>Calend-Ez</h2>
-      <ul>
-        <li className="active">Event types</li>
-      </ul>
+      <nav>
+
+        <Link to={`/success/events/${email}`}>Event Types</Link>
+        <Link to={`/success/appointments/${email}`}>Appointments</Link>
+      </nav>
+  
     </div>
   );
-}
+};
